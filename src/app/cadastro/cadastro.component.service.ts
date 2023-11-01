09c2c926a3f87e9203ec2325e6764932b83a66be
Hugo Friedrich <86070257+Hugofriedrich369@users.noreply.github.com>
2023-11-01 17:17:00 -0300

    [AulaService] CRUD atualizado 1.1.0

--- src/app/cadastro/cadastro.component.service.ts
+++ src/app/cadastro/cadastro.component.service.ts
@@ -1,28 +1,45 @@
-import { Usuario } from "./IUsuario";
+import { Usuario } from './IUsuario';
 
 export class CadastroService {
   usuarios: Usuario[] = [
     {
-      nome: "Hugo",
-      sobrenome: "Friedrich",
-      idade: "18 anos",
-      peso: "60 kg"
-    }
+      nome: 'Hugo',
+      sobrenome: 'Friedrich',
+      idade: '18',
+      peso: '60',
+    },
   ];
 
   addUser(nome: string, sobrenome: string, idade: string, peso: string) {
-
     const usuarioNovo: Usuario = {
       nome: nome,
       sobrenome: sobrenome,
-      idade: idade + " anos",
-      peso: peso + " kg",
-    }
+      idade: idade,
+      peso: peso,
+    };
 
     this.usuarios.push(usuarioNovo);
   }
 
   removeUser(index: number) {
     this.usuarios.splice(index, 1);
+  }
+  getUser(index: number): Usuario {
+    return this.usuarios[index];
+  }
+  editUser(
+    nomeEdicao: string,
+    sobrenomeEdicao: string,
+    idadeEdicao: string,
+    pesoEdicao: string,
+    index: number
+  ) {
+    const usuario: Usuario = {
+      nome: nomeEdicao,
+      sobrenome: sobrenomeEdicao,
+      idade: idadeEdicao,
+      peso: pesoEdicao,
+    };
+    this.usuarios[index] = usuario;
   }
 }
--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -13,38 +13,70 @@ export class CadastroComponent {
   idade!: string;
   peso!: string;
   listaUsuarios!: Usuario[];
-  type!: string;
+  indexEdicao!: number;
 
+
+  type!: string;
   message!: string;
   messageStatus!: boolean;
+
 
   constructor(private cadastroService: CadastroService) {
     this.listaUsuarios = this.cadastroService.usuarios;
   }
 
   addUser() {
-    this.cadastroService.addUser(this.nome, this.sobrenome, this.idade, this.peso);
-    this.clearDados()
+    this.cadastroService.addUser(
+      this.nome,
+      this.sobrenome,
+      this.idade,
+      this.peso
+    );
+    this.clearDados();
     this.listaUsuarios = this.cadastroService.usuarios;
-    this.status(`Usuário ${this.nome} adicionado com sucesso!`, true, "success")
+    this.status(
+      `Usuário ${this.nome} adicionado com sucesso!`,
+      true,
+      'success'
+    );
   }
 
   removeUser(index: number) {
-    console.log(index)
+    console.log(index);
     this.cadastroService.removeUser(index);
-    this.status(`Usuário removido com sucesso!`, true, "danger")
+    this.status(`Usuário removido com sucesso!`, true, 'danger');
   }
 
   clearDados() {
     this.nome = '';
     this.sobrenome = '';
-    this.idade = "";
-    this.peso = "";
+    this.idade = '';
+    this.peso = '';
   }
 
   status(message: string, status: boolean, type: string) {
     this.message = message;
     this.messageStatus = status;
     this.type = type;
+  }
+
+  edicao(index: number) {
+    const usuarioEdicao = this.cadastroService.getUser(index);
+    this.nome = usuarioEdicao.nome;
+    this.sobrenome = usuarioEdicao.sobrenome;
+    this.idade = usuarioEdicao.idade;
+    this.peso = usuarioEdicao.peso;
+    this.indexEdicao = index;
+  }
+  editarUser() {
+    this.cadastroService.editUser(
+      this.nome,
+      this.sobrenome,
+      this.idade,
+      this.peso,
+      this.indexEdicao
+    );
+    this.status(`Usuário ${this.nome} atualizado com sucesso!`, true, 'warning');
+    this.clearDados();
   }
 }
